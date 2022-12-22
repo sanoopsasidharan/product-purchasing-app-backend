3fb1ca5ad5be7525bf107fe267c681d20a761e8e
sanoopsasidharan <sanoopsanu122@gmail.com>
2022-12-22 10:50:42 +0530

    db connected

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+const createError = require("http-errors");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// config dotenv file
+require("dotenv").config();
+// connecting mongodb
+require("./config/connection");
+
+var prodectRouter = require("./routes/prodect");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/prodect", prodectRouter);
+
+// throw error
+app.use((req, res, next) => next(createError.NotFound()));
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
 
 module.exports = app;
